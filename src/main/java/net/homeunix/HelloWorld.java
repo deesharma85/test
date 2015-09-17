f9d5a3c9314a1b1db644a346563cfc15e08312fc
deepak S <s.deepak@blrmcb.local>
2015-09-17 20:23:16 +0530

    helloworld

---
+++ src/main/java/net/homeunix/HelloWorld.java
@@ -0,0 +1,10 @@
+package net.homeunix;
+
+/**
+ * Created by s.deepak on 17/09/15.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
